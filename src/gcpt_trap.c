efcc0cd6b87089ffbc1462f8a94e64a9eb276bd0
jiaxiaoyu <xyyy1420@gmail.com>
2025-03-18 19:05:35 +0800

    feat: replace printf with mt_printf
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- src/gcpt_trap.c
+++ src/gcpt_trap.c
@@ -1,15 +1,15 @@
 #include "gcpt_trap.h"
 #include "encoding.h"
 #include "gcpt_asm.h"
-#include "printf.h"
+#include "utils.h"
 #include <stdint.h>
 
 void __attribute__((section(".text.gcpt_trap_handler"))) gcpt_trap_handler() {
-  printf("mepc:    0x%lx\n", read_csr(mepc)); // mepc
-  printf("mtval    0x%lx\n", read_csr(mtval)); // mtval
-  printf("mcause   0x%lx\n", read_csr(mcause)); // mcause
-  printf("mstatus  0x%lx\n", read_csr(mstatus)); // mstatus
-  printf("Get gcpt trap\n");
+  mt_printf("mepc:    0x%lx\n", read_csr(mepc)); // mepc
+  mt_printf("mtval    0x%lx\n", read_csr(mtval)); // mtval
+  mt_printf("mcause   0x%lx\n", read_csr(mcause)); // mcause
+  mt_printf("mstatus  0x%lx\n", read_csr(mstatus)); // mstatus
+  mt_printf("Get gcpt trap\n");
   nemu_signal(GCPT_GET_TRAP);
 }
 
